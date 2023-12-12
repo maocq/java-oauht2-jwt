8298e123422fa29bfeb3170147cc247c2391305f
maocq <johncarm@bancolombia.com.co>
2023-12-12 16:17:19 -0500

    Jwt

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # Java OAuth2 Jwt
 
+- Front
+  https://github.com/Azure-Samples/ms-identity-javascript-v2
+
+/app/authConfig.js
+```
+clientId: "78cdc600-c99c-4db3-9556-3eb31a4c0c60",
+authority: "https://login.microsoftonline.com/e7c91a18-425d-4820-aedf-09d9c1c63cd7",
+redirectUri: "http://localhost:3000"
+```
+
+npm start
+
+Get jwt - Application/Storage/Local Storage/
+
+credentialType:"IdToken"
+secret
+
+- Validación
+```sh
+curl --location --request GET 'http://localhost:8080/api/usecase/path' \
+--header 'Content-Type: application/json' \
+--header 'Authorization: Bearer ...'
+```
--- infrastructure/entry-points/reactive-web/src/main/java/co/com/bancolombia/api/Handler.java
+++ infrastructure/entry-points/reactive-web/src/main/java/co/com/bancolombia/api/Handler.java
@@ -1,6 +1,7 @@
 package co.com.bancolombia.api;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
@@ -12,7 +13,14 @@ public class Handler {
     //private  final UseCase useCase;
 
     public Mono<ServerResponse> listenGETUseCase(ServerRequest serverRequest) {
-        return ServerResponse.ok().bodyValue("Hello");
+        return serverRequest.principal()
+                .flatMap(principal -> {
+                    if (principal instanceof Authentication authentication) {
+                        System.out.println(authentication.getAuthorities());
+                    }
+
+                    return ServerResponse.ok().bodyValue(principal.getName());
+                });
     }
 
     public Mono<ServerResponse> listenGETOtherUseCase(ServerRequest serverRequest) {
